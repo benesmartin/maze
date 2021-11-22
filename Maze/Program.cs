48a330f40cd44855921ddec86c53c9963de28656
benesmartin <martinbenes790@gmail.com>
2021-11-22 13:38:20 +0100

    create array canvas

--- Maze/Program.cs
+++ Maze/Program.cs
@@ -6,7 +6,27 @@ namespace Maze
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size = 11;
+            var znakI = "#";
+            var znakJ = "-";
+            var pole = new string[size, size];
+            for (int i = 0; i < pole.GetLength(0); i++)
+            {
+                for (int j = 0; j < pole.GetLength(1); j++)
+                {
+                    if(j == 0 || j == size - 1 || i == 0 || i == size - 1) 
+                    {
+                        pole[i, j] = znakI;
+                    }
+                    else
+                    {
+                        pole[i, j] = znakJ;
+                    }  
+                    Console.Write("{0} ", pole[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
